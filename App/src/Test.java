3f586a0a5adbbe8802a0da9f68522284dde02ff1
whigon <1054733646@qq.com>
2020-02-23 00:27:03 +0800

    For test

---
+++ App/src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        Main m = new Main();
+        String result = m.translate("13213219879");
+        String r1 = m.getResult();
+        System.out.println(result);
+        System.out.println(r1);
+    }
+}
